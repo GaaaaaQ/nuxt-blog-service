534689aa4f964772a333cd583558a8d56243ba18
GaaaaaQ <mod.ulo831@gmail.com>
2019-05-28 17:51:16 +0900

    first commit.

---
+++ app/store/index.js
@@ -0,0 +1,30 @@
+export const state = () => ({
+  isLoggedIn: false,
+  user: null
+})
+
+export const getters = {
+  isLoggedIn: (state) => state.isLoggedIn,
+  user: (state) => state.user
+}
+
+export const mutations = {
+  setUser(state, { user }) {
+    state.user = user
+    state.isLoggedIn = true
+  }
+}
+
+export const action = {
+  async login({ commit }, { id }) {
+    const user = await this.$axios.$get(`/users/${id}.json`)
+    commit('setUser', { user })
+  },
+  async register({ commit }, { id }) {
+    const payload = {}
+    await this.$axios.$patch(`/users.json`, payload)
+    const user = await this.$axios.$get(`/users/${id}.json`)
+    if (!user.id) throw new Error('Invalid user')
+    commit('setUser', { user })
+  }
+}
